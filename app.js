a7cf996eb2d6c66a7acafc93cb57b437a4cbed40
johnmicahmiguel <johnmicahmiguel@gmail.com>
2021-05-29 22:59:57 +0800

    put static port for now

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.get('/', function (req, res) {
     res,status(200).send({'message': "Welcome to NodeJS API"});
 })
  
-app.listen(process.env.PORT, ()=>{console.log("Server started!")});
+app.listen(5000, ()=>{console.log("Server started!")});
 
