232701663f6dbbe25a981a7923427bcf61da58df
johnmicahmiguel <johnmicahmiguel@gmail.com>
2021-05-29 23:08:29 +0800

    Fix status error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-    res,status(200).send({'message': "Welcome to NodeJS API"});
+    res.status(200).send({'message': "Welcome to NodeJS API"});
 })
  
 app.listen(process.env.PORT || 8080, ()=>{console.log("Server started!")});
