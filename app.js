4fc77daa0aa166db87e7ca36f7a7fc242f72d057
johnmicahmiguel <johnmicahmiguel@gmail.com>
2021-05-29 23:31:04 +0800

    Try database connection

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
  
+mongoose.connect("mongodb://root:5m@rtw@v3@srv-captain--mongo-sample/mydatabase?authSource=admin", { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+.then(() => {console.log('MongoDB successfully connected!')}, err => {console.log('Something went wrong!')});
+
 app.get('/', function (req, res) {
     res.status(200).send({'message': "Welcome to NodeJS API"});
 })
