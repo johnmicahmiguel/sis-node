68d514dd4800db37cf5e6d4e98ece913b6403af3
johnmicahmiguel <johnmicahmiguel@gmail.com>
2021-05-29 23:05:35 +0800

    change port options

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.get('/', function (req, res) {
     res,status(200).send({'message': "Welcome to NodeJS API"});
 })
  
-app.listen(5000, ()=>{console.log("Server started!")});
+app.listen(process.env.PORT || 8080, ()=>{console.log("Server started!")});
 
